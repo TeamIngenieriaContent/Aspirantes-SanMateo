81fbc75e063d15d506b543e7787f4680b51a2869
Diego Mogollón <diego.mogollon.camacho@gmail.com>
2023-02-20 10:42:33 -0500

    feat: Modification of the Home view with iFrame for the display of the citadel. Adding functionality to the main menu.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,11 @@
 <x-app-layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+    <div class="py-5">
+        <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg" x-data="{ open: false }">
+{{--                 <button x-on:click="open = true" class="w-full aspect-video">
+                    <img src="{{Storage::url('img/Mateverso.jpg')}}" class="w-full h-full">
+                </button> --}}
+                <iframe loading="lazy" class="w-full aspect-video" src="https://moodlevirtual.sanmateovirtual.edu.co/ModulosVirtuales/Prueba/Mateverso2/index.html"></iframe>
             </div>
         </div>
     </div>
---
+++ resources/views/user_experience.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <div class="py-5">
+        <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            </div>
+        </div>
+    </div>
+</x-app-layout>
